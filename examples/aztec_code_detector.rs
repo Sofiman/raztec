6846c5b6774c57a507fc4a9d79df28d6671e854a
Sofiane <sofiane.meftah@epita.fr>
2022-05-16 19:32:29 +0200

    Started aztec code center checks

--- examples/aztec_code_detector.rs
+++ examples/aztec_code_detector.rs
@@ -9,26 +9,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
     let img = image::open(args.get(1).unwrap()).unwrap().into_luma8();
-    let mut pixels = Vec::new();
-    for (_x, _y, p) in img.enumerate_pixels() {
-        pixels.push(p[0]);
-    }
+    let pixels: Vec<u8> = img.enumerate_pixels().map(|(_, _, p)| p[0]).collect();
     let mut reader = AztecReader::from_grayscale(img.dimensions(), &pixels);
-    reader.read();
+
+    match reader.read() {
+        Ok(code) => println!("Aztec Code Found {:?}", code),
+        Err(kind) => println!("{}", kind)
+    }
 
     let (width, height) = img.dimensions();
-    let pixels: Vec<u8> = pixels.iter().map(|&x| x <= 104)
-        .map(|x| if x { 0 } else { 255 }).collect();
-    let image = ImageView::new(ImageInfo::mono8(width, height), &pixels);
-
-    let mut pixels: Vec<u32> = pixels.iter().map(|&x| x <= 51)
+    let mut pixels: Vec<u32> = pixels.iter().map(|&x| x <= 104)
         .map(|x| if x { 0 } else { 0xffffff }).collect();
     for marker in reader.markers.iter() {
-        let (row, col) = marker.loc;
+        let (col, row) = marker.loc;
         let (w, h) = marker.size;
         for i in row..(row+h) {
             for j in col..(col+w) {
-                pixels[i * width as usize + j] = (pixels[i * width as usize + j] + marker.color) / 2;
+                pixels[i * width as usize + j] = marker.color;
             }
         }
     }
@@ -39,8 +36,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Create a window with default options and display the image.
     let window = create_window("image", Default::default())?;
-    //window.set_image("image-001", image)?;
-    window.set_image("marked", marked)?;
+    window.set_image("result", marked)?;
 
     for event in window.event_channel()? {
       if let event::WindowEvent::KeyboardInput(event) = event {
--- src/reader/mod.rs
+++ src/reader/mod.rs
@@ -1,22 +1,58 @@
-#[derive(Debug, PartialEq)]
+//! Aztec Reader module
+
+use std::fmt::Display;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+/// Represents the state in which the AztecReader has failed
 pub enum AztecReadError {
     NotFound,
 }
 
+impl AztecReadError {
+    /// Returns the error message corresponding to the current error
+    pub fn message(&self) -> &str {
+        use AztecReadError::*;
+        match self {
+            NotFound => "No Aztec Code was found"
+        }
+    }
+}
+
+impl Display for AztecReadError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "AztecReadError({})", self.message())
+    }
+}
+
 pub struct Marker {
     pub color: u32,
+    /// loc: (col, row)
     pub loc: (usize, usize),
+    /// size: (width, height)
     pub size: (usize, usize)
 }
 
 impl Marker {
-    pub fn red((row, col): (usize, usize), (width, height): (usize, usize)) -> Self {
-        Marker { color: 0xff0000, loc: (row, col), size: (width, height) }
+    pub fn red(loc: (usize, usize), size: (usize, usize)) -> Self {
+        Marker { color: 0xff0000, loc, size }
     }
 
-    pub fn orange((row, col): (usize, usize), (width, height): (usize, usize)) -> Self {
-        Marker { color: 0xffb86c, loc: (row, col), size: (width, height) }
+    pub fn orange(loc: (usize, usize), size: (usize, usize)) -> Self {
+        Marker { color: 0xffb86c, loc, size }
     }
+
+    pub fn green(loc: (usize, usize), size: (usize, usize)) -> Self {
+        Marker { color: 0x00ff00, loc, size }
+    }
+
+    pub fn blue(loc: (usize, usize), size: (usize, usize)) -> Self {
+        Marker { color: 0x0000ff, loc, size }
+    }
+}
+
+struct AztecCenter {
+    loc: (usize, usize),
+    mod_size: f32
 }
 
 pub struct AztecReader {
@@ -26,58 +62,125 @@ pub struct AztecReader {
     pub markers: Vec<Marker>
 }
 
-fn grayscale(rgb: &u32) -> u8 {
-    let   red = (rgb & 0xFF0000) as f32;
-    let green = (rgb & 0x00FF00) as f32;
-    let  blue = (rgb & 0x0000FF) as f32;
-    ((0.2126 * red + 0.7152 * green + 0.0722 * blue) * 255.0).round() as u8
-}
-
 impl AztecReader {
-    pub fn from_rgb((width, height): (u32, u32), rgb: &[u32]) -> AztecReader {
+
+    /// Create a new AztecReader struct from a grayscale image
+    /// 
+    /// # Arguments
+    /// * (`w`, `h`): The width and height of the grayscale image
+    /// * `mono`: The grayscale pixel array (8 bits)
+    pub fn from_grayscale((w, h): (u32, u32), mono: &[u8]) -> AztecReader {
         AztecReader { 
-            width: width as usize, height: height as usize,
-            image: rgb.iter().map(grayscale).map(|x| x < 104).collect(),
+            width: w as usize, height: h as usize,
+            image: mono.iter().map(|&x| x < 104).collect(),
             markers: vec![]
         }
     }
 
-    pub fn from_grayscale((width, height): (u32, u32), rgb: &[u8]) -> AztecReader {
-        AztecReader { 
-            width: width as usize, height: height as usize, 
-            image: rgb.iter().map(|&x| x < 104).collect(),
-            markers: vec![]
-        }
-    }
-
-    fn check_ratio(&self, counts: &[usize; 9]) -> bool {
-        let mut total = 0;
-        for &count in counts {
-            if count == 0 {
-                return false;
-            }
-            total += count;
-        }
-
+    fn check_ratio(&self, counts: &[usize; 9], total: usize) -> bool {
         if total < 11 {
             return false;
         }
-
-        let mod_size = (total / 11 + if total % 11 != 0 { 1 } else { 0 }) as i32;
+        let mod_size = ((total - 1) / 11) as i32 + 1;
         let max_v = mod_size as i32 / 2;
 
-        total = 0;
-        let mut i = 0;
-        while i < 9 {
-            if (mod_size - counts[i] as i32).abs() < max_v {
+        let mut total = 0;
+        for &count in counts {
+            if (mod_size - count as i32).abs() < max_v {
                 total += 1;
             }
-            i += 1;
         }
         total >= 8
     }
 
-    fn find_bullseye(&mut self) -> Result<(usize, usize), AztecReadError> {
+    fn count_states(&self, start_row: usize, start_col: usize, drow: usize, 
+        dcol: usize, row_out: &mut usize, col_out: &mut usize) -> [usize; 9] {
+        let mut current_state = 0;
+        let mut counts = [0usize; 9];
+        let mut row = start_row;
+        let mut col = start_col;
+        loop {
+            if row >= self.height || col >= self.width {
+                break;
+            }
+
+            if self.image[row * self.width + col] { // black pixel
+                if current_state % 2 == 1 {
+                    current_state += 1;
+                }
+                counts[current_state] += 1;
+            } else if current_state % 2 == 1 {
+                counts[current_state] += 1;
+            } else if current_state == 8 {
+                break;
+            } else {
+                current_state += 1;
+                counts[current_state] += 1;
+            }
+
+            row += drow;
+            col += dcol;
+        }
+        *row_out = row;
+        *col_out = col;
+        counts
+    }
+
+    fn center_from_end(&self, counts: &[usize; 9], col: usize) -> usize {
+        (col-counts[5..].iter().sum::<usize>())-counts[4] / 2
+    }
+
+    fn check_vertical(&self, start_row: usize, col: usize, mid: usize, total: usize) -> Option<usize> {
+        let (mut new_row, mut new_col) = (0, 0);
+        let counts = self.count_states(start_row, col, 1, 0, &mut new_row, &mut new_col);
+        let total = total as i32;
+        let new_total = counts.iter().sum::<usize>();
+        if 9 * (total - new_total as i32).abs() < total &&
+            self.check_ratio(&counts, new_total) {
+            Some(self.center_from_end(&counts, new_row))
+        } else {
+            None
+        }
+    }
+
+    fn check_horizontal(&self, row: usize, col: usize, mid: usize, total: usize) -> Option<usize> {
+        todo!()
+    }
+
+    fn check_diag(&self, row: usize, col: usize, mid: usize, total: usize) -> bool {
+        todo!()
+    }
+
+    fn handle_center(&mut self, counts: &[usize; 9], row: usize, col: usize,
+        total: usize, centers: &mut Vec<AztecCenter>) -> Option<()> {
+
+        let mid_val = counts[4];
+        println!("verifying center...");
+        let center_col = self.center_from_end(counts, col);
+        self.markers.push(Marker::orange((center_col, row), (1, 1)));
+        let r = row - counts[..4].iter().sum::<usize>();
+        let center_row = self.check_vertical(r, center_col, mid_val, total)?;
+        println!("vert OK");
+        self.markers.push(Marker::red((center_col, center_row), (1, 1)));
+        Some(())
+
+        /*
+        let center_col = self.check_horizontal(center_row,
+            center_col, mid_val, total)?;
+
+        if self.check_diag(center_row, center_col, counts[2], total) {
+            let mod_size = total as f32 / 11.0;
+            let center = AztecCenter { loc: (center_col, center_row), mod_size };
+            // TODO: Check if there is any close enough centers
+            centers.push(center);
+            Some(())
+        } else {
+            None
+        }*/
+    }
+
+    fn find_bullseye(&mut self) -> Result<AztecCenter, AztecReadError> {
+        let mut centers = vec![];
         let mut counts = [0; 9];
         for row in 0..self.height {
             counts.fill(0);
@@ -92,9 +195,10 @@ impl AztecReader {
                 } else if current_state % 2 == 1 {
                     counts[current_state] += 1;
                 } else if current_state == 8 {
-                    if self.check_ratio(&counts) {
-                        let w = counts.iter().sum();
-                        self.markers.push(Marker::red((row, col - w), (w, 1)));
+                    let total: usize = counts.iter().sum();
+                    if self.check_ratio(&counts, total) {
+                        self.handle_center(&counts, row, col, total, &mut centers);
+                        //println!("check_ratio returned true");
                         counts.fill(0);
                         current_state = 0;
                     } else {
@@ -109,11 +213,16 @@ impl AztecReader {
                 }
             }
         }
-        Result::Err(AztecReadError::NotFound)
+        if centers.is_empty() {
+            Err(AztecReadError::NotFound)
+        } else {
+            Ok(centers.remove(0))
+        }
     }
 
     pub fn read(&mut self) -> Result<String, AztecReadError> {
-        let (center_x, center_y) = self.find_bullseye()?;
+        let center = self.find_bullseye()?;
+        let (center_x, center_y) = center.loc;
         println!("center: (x: {}, y: {})", center_x, center_y);
         todo!();
     }
