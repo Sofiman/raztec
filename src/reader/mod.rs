bc2d018bed19dc7004d6c5027df043ae9ffa66c3
Sofiane <sofiane.meftah@epita.fr>
2022-05-27 23:19:46 +0200

    Fixed Aztec code center checks

--- src/reader/mod.rs
+++ src/reader/mod.rs
@@ -55,6 +55,16 @@ struct AztecCenter {
     mod_size: f32
 }
 
+fn div_ceil(a: isize, b: isize) -> isize {
+    let d = a / b;
+    let r = a % b;
+    if (r > 0 && b > 0) || (r < 0 && b < 0) {
+        d + 1
+    } else {
+        d
+    }
+}
+
 pub struct AztecReader {
     width: usize,
     height: usize,
@@ -78,20 +88,17 @@ impl AztecReader {
     }
 
     fn check_ratio(&self, counts: &[usize; 5], total: usize) -> bool {
-        if total < 7 {
+        if total < 5 {
             return false;
         }
-        let mod_size = ((total - 1) / 7) as i32 + 1;
-        let max_v = mod_size as i32 / 2;
-        println!("{:?}", counts);
+        let mod_size = div_ceil(total as isize, 5);
+        let max_v = mod_size / 2;
 
-        let mut total = 0;
-        for &count in counts {
-            if (mod_size - count as i32).abs() < max_v {
-                total += 1;
-            }
+        let mut i = 0;
+        while i < 5 && (mod_size - counts[i] as isize).abs() < max_v {
+            i += 1;
         }
-        total == 5
+        i == 5
     }
 
     fn center_from_end(&self, counts: &[usize; 5], col: usize) -> usize {
@@ -147,8 +154,7 @@ impl AztecReader {
 
         let total = total as i32;
         let new_total: usize = counts.iter().sum();
-        if 5 * (new_total as i32 - total).abs() < 2 * total &&
-            self.check_ratio(&counts, new_total) {
+        if self.check_ratio(&counts, new_total) {
             Some(self.center_from_end(&counts, row - 1))
         } else {
             None
@@ -219,7 +225,7 @@ impl AztecReader {
     fn handle_center(&mut self, counts: &[usize; 5], row: usize, col: usize,
         total: usize, centers: &mut Vec<AztecCenter>) -> Option<()> {
 
-        let mid_val = (counts.iter().sum::<usize>() as f32 / 5.0).ceil() as usize;
+        let mid_val = counts[2] + counts[3] / 2;
         println!("verifying center...");
         let center_col = self.center_from_end(counts, col);
         let center_row = self.check_vertical(row, center_col, mid_val, total)?;
@@ -242,7 +248,7 @@ impl AztecReader {
         }*/
     }
 
-    fn find_bullseye(&mut self) -> Result<AztecCenter, AztecReadError> {
+    fn find_bullseyes(&mut self) -> Vec<AztecCenter> {
         let mut centers = vec![];
         let mut counts = [0; 5];
         for row in 0..self.height {
@@ -259,11 +265,11 @@ impl AztecReader {
                     counts[current_state] += 1;
                 } else if current_state == 4 {
                     let total: usize = counts.iter().sum();
-                    if self.check_ratio(&counts, total) {
-                        self.markers.push(Marker::red((col, row), (1, 1)));
-                        self.handle_center(&counts, row, col, total, &mut centers);
-                        counts.fill(0);
-                        current_state = 0;
+                    if self.check_ratio(&counts, total) &&
+                        self.handle_center(&counts, row, col, total,
+                            &mut centers).is_some() {
+                            counts.fill(0);
+                            current_state = 0;
                     } else {
                         current_state = 3;
                         counts.rotate_left(2);
@@ -276,11 +282,7 @@ impl AztecReader {
                 }
             }
         }
-        if centers.is_empty() {
-            Err(AztecReadError::NotFound)
-        } else {
-            Ok(centers.remove(0))
-        }
+        centers
     }
 
     fn get_px(&self, row: usize, col: usize) -> bool {
@@ -288,9 +290,14 @@ impl AztecReader {
     }
 
     pub fn read(&mut self) -> Result<String, AztecReadError> {
-        let center = self.find_bullseye()?;
-        let (center_x, center_y) = center.loc;
-        println!("center: (x: {}, y: {})", center_x, center_y);
+        let centers = self.find_bullseyes();
+        if centers.is_empty() {
+            return Err(AztecReadError::NotFound);
+        }
+        for center in centers {
+            let (center_x, center_y) = center.loc;
+            println!("center: (x: {}, y: {})", center_x, center_y);
+        }
         todo!();
     }
 }
